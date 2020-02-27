0493373523eb432071a3521159051cf41abade75
Brandon Walker <brandoneugenewalker@outlook.com>
2020-02-26 19:04:21 -0500

    Added MatrixMath to server project.
    MatrixMath has static methods for performing matrix math problems.
    Only includes MatrixMath.multiply(a, b) right now.

--- CS4225HW4Group6_Server/src/edu/westga/cs4225/main/Main.java
+++ CS4225HW4Group6_Server/src/edu/westga/cs4225/main/Main.java
@@ -1,8 +1,10 @@
+package edu.westga.cs4225.main;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ CS4225HW4Group6_Server/src/edu/westga/cs4225/model/MatrixMath.java
@@ -0,0 +1,40 @@
+package edu.westga.cs4225.model;
+
+/**
+ * 
+ * This class handles math problems for Matrix objects.
+ * 
+ * @author Brandon Walker, Kevin Flynn, Luke Whaley
+ *
+ */
+public class MatrixMath {
+
+	/**
+	 * Multiplies two matrixes and returns the resulting matrix.
+	 * @precondition the matrixes cannot be null
+	 * 
+	 * @param matrixA
+	 * @param matrixB
+	 * @return
+	 */
+    public static Matrix multiply(Matrix matrixA, Matrix matrixB) {
+    	if (matrixA == null) {
+    		throw new IllegalArgumentException("Matrix A cannot be null!");
+    	}
+    	if (matrixB == null) {
+    		throw new IllegalArgumentException("Matrix B cannot be null!");
+    	}
+    	
+    	Matrix matrixC = new Matrix('C', matrixA.getNumberOfRows(), matrixB.getNumberOfColumns());
+    	for (int i = 0; i < matrixA.getNumberOfColumns(); i++) {
+    		for (int j = 0; j < matrixB.getNumberOfColumns(); j++) {
+    			for (int k = 0; k < matrixA.getNumberOfColumns(); k++) {
+    				int value = matrixA.getMatrixCellValue(i, k) * matrixB.getMatrixCellValue(k, j);
+    				matrixC.setMatrixCellValue(i, j, value);
+    			}
+    		}
+    	}
+    	return matrixC;
+    }
+    
+}
