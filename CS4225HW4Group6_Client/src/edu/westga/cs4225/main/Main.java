cbabf7ec20d3a7a275fa93bc00d4ca9daad7264b
Thomas Whaley <twhaley1@my.westga.edu>
2020-03-01 17:55:13 -0500

    Now only saves when the output file can be saved to. Previously threw SecurityException when trying to save somewhere without permission.

--- CS4225HW4Group6_Client/src/edu/westga/cs4225/main/Main.java
+++ CS4225HW4Group6_Client/src/edu/westga/cs4225/main/Main.java
@@ -32,17 +32,11 @@ public class Main {
 	public static void main(String[] args) {
 		File inputFile = new File(args[0]);
 		File outputFile = new File(args[1]);
-		if (!inputFile.exists() || !inputFile.canRead()) {
-			System.err.println("Invalid Input File..");
-			System.exit(1);
-		}
-		try {
-			outputFile.createNewFile();
-		} catch (IOException e1) {
-			System.err.println("The output file could not be created at: " + outputFile.getAbsolutePath());
-		}
-		Operands operands = null;
-		operands = buildOperand(inputFile, operands);
+		
+		validateInputFile(inputFile);
+		boolean shouldSave = validateOutputFile(outputFile);
+		Operands operands = buildOperands(inputFile);
+		
 		MatrixClient client = new MatrixClient(HOST, PORT);
 		try {
 			System.out.println("Client Starting...");
@@ -54,17 +48,38 @@ public class Main {
 				System.out.println(consoleDisplay);
 			}
 
-			MatrixFileSaver.saveMatrixResults(outputFile, productMatrix, performanceStats);
-			String fileOutputResults = "The results of the operation were saved at: " + System.lineSeparator()
-					+ outputFile.getAbsolutePath();
-			System.out.println(fileOutputResults);
-
+			if (shouldSave) {
+				MatrixFileSaver.saveMatrixResults(outputFile, productMatrix, performanceStats);
+				String fileOutputResults = "The results of the operation were saved at: " + System.lineSeparator()
+						+ outputFile.getAbsolutePath();	
+				System.out.println(fileOutputResults);
+			}
 		} catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static Operands buildOperand(File inputFile, Operands operands) {
+	private static void validateInputFile(File inputFile) {
+		if (!inputFile.exists() || !inputFile.canRead()) {
+			System.err.println("Invalid Input File..");
+			System.exit(1);
+		}
+	}
+	
+	private static boolean validateOutputFile(File outputFile) {
+		boolean shouldSave = false;
+		try {
+			outputFile.createNewFile();
+			shouldSave = true;
+		} catch (IOException e1) {
+			System.err.println("The output file could not be created at: " + outputFile.getAbsolutePath());
+			System.err.println("The results will not be saved.");
+		}
+		return shouldSave;
+	}
+	
+	private static Operands buildOperands(File inputFile) {
+		Operands operands = null;
 		try {
 			MatrixFileReader reader = new MatrixFileReader();
 			operands = reader.readFile(inputFile);
